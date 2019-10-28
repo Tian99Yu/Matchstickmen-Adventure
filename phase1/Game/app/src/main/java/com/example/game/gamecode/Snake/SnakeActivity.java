f1b86d48b53f90549053adc902193b9ed457c172
yutian <tiancharles.yu@mail.utoronto.ca>
2019-10-27 22:42:46 -0400

    create a new class SnakeActivity. Functionality is like the MainActivity

---
+++ phase1/Game/app/src/main/java/com/example/game/gamecode/Snake/SnakeActivity.java
@@ -0,0 +1,57 @@
+package com.example.game.gamecode.Snake;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.game.R;
+import com.example.game.gamecode.GameActivity;
+import com.example.game.gamecode.GameView;
+
+public class SnakeActivity extends GameActivity {
+
+
+    Button LeftButton;
+    Button RightButton;
+    SnakeHead snakehead;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.snake_layout);
+
+        LeftButton = findViewById(R.id.LeftButton);
+        RightButton = findViewById(R.id.RightButton);
+
+
+        View.OnClickListener LeftListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                snakehead.turn(TurnDirection.LEFT);
+            }
+        };
+
+        LeftButton.setOnClickListener(LeftListener);
+
+
+        View.OnClickListener RightListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snakehead.turn(TurnDirection.RIGHT);
+            }
+        };
+
+        RightButton.setOnClickListener(RightListener);
+    }
+
+
+    protected GameView setView() {
+        return null;
+    }
+
+}
