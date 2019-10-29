750f98c3448fec1d20c1684ec42b51c5f704ebc7
yutian <tiancharles.yu@mail.utoronto.ca>
2019-10-28 22:30:16 -0400

    fixed errors and added canvas for snakeactivity

--- phase1/Game/app/src/main/java/com/example/game/gamecode/Snake/SnakeActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/gamecode/Snake/SnakeActivity.java
@@ -3,6 +3,7 @@ package com.example.game.gamecode.Snake;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,33 +23,42 @@ public class SnakeActivity extends GameActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        setContentView(R.layout.snake_layout);
-//
-//        LeftButton = findViewById(R.id.LeftButton);
-//        RightButton = findViewById(R.id.RightButton);
-//
-//
-//        View.OnClickListener LeftListener = new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                snakehead.turn(TurnDirection.LEFT);
-//            }
-//        };
-//
-//        LeftButton.setOnClickListener(LeftListener);
-//
-//
-//        View.OnClickListener RightListener = new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                snakehead.turn(TurnDirection.RIGHT);
-//            }
-//        };
-//
-//        RightButton.setOnClickListener(RightListener);
-    }
+        setContentView(R.layout.snake_layout);
 
+
+        LeftButton = findViewById(R.id.LeftButton);
+        RightButton = findViewById(R.id.RightButton);
+
+
+        //add the gameview as a second layout:
+        gameView = this.setView();
+        FrameLayout frameLayout =  findViewById(
+                R.id.snake_game
+
+        );
+        frameLayout.addView(
+                gameView
+        );
+
+        View.OnClickListener LeftListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snakehead.turn(TurnDirection.LEFT);
+            }
+        };
+
+        LeftButton.setOnClickListener(LeftListener);
+
+
+        View.OnClickListener RightListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snakehead.turn(TurnDirection.RIGHT);
+            }
+        };
+
+        RightButton.setOnClickListener(RightListener);
+    }
 
     protected GameView setView() {
         return null;
